cb2a4280bd74ff865e7817bb9b9e31fdcc51f415
Jason Garcia <jaygarcia0530@gmail.com>
2023-11-13 20:16:25 -0800

    removed timecard

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -55,13 +55,5 @@ export const ProjectData = [
         description: "Halloween themed multiplayer blackjack game. Users take turns drawing cards to sum their hand to 21. Focused around object oriented programming and functional programming. Custom methods for drawing cards, adding cards to hand, and ending user turn.",
         codeLink: "https://github.com/JasonGarcia131/Halloween-BlackJack",
         pageLink: "https://jasongarcia131.github.io/Halloween-BlackJack/"
-    },
-    {
-        imgUrl: img13,
-        name: "Time-Card-Calculator",
-        techStack: "React",
-        description: "This app calculates employee hours worked in a week. Simple frontend project with a custom time conversion function for employee punch in/ punch out.",
-        codeLink: "https://github.com/JasonGarcia131/Time-Card-Calculator",
-        pageLink: "https://jasongarcia131.github.io/Time-Card-Calculator/"
-    },
+    }
 ]
